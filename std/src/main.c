d6bc39c1ba272e8964cd0cc197934d82bf6e82c1
jun <junchen@151.com>
2020-04-12 10:46:38 -0400

    simple allocator

---
+++ std/src/main.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<sys/malloc.h>
+#include<sys/params.h>
+#include<sys/types.h>
+#include<sys.module.h>
+//int start();
+
+int main() {
+
+    printf("Hello world!\n");
+    printf("Number is %d.\n", start());
+    return 0;
+
+}
