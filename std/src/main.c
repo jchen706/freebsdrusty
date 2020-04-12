d225e8f3e6c7907aaad980b965fa719fa527c53d
jun <junchen@151.com>
2020-04-12 11:18:49 -0400

    simple allocator

--- std/src/main.c
+++ std/src/main.c
@@ -1,14 +1,18 @@
-#include<stdio.h>
-#include<sys/malloc.h>
-#include<sys/params.h>
-#include<sys/types.h>
-#include<sys.module.h>
-//int start();
+#include <sys/types.h>
+#include <sys/module.h>
+#include <sys/systm.h>  /* uprintf */
+#include <sys/param.h>  /* defines used in kernel.h */
+#include <sys/kernel.h> /* types used in module initialization */
+#include <sys/conf.h>   /* cdevsw struct */
+#include <sys/uio.h>    /* uio struct */
+#include <sys/malloc.h>
+#include <sys/kthread.h>
+#include <sys/unistd.h>//int start();
 
 int main() {
 
     printf("Hello world!\n");
-    printf("Number is %d.\n", start());
+    //printf("Number is %d.\n", start());
     return 0;
 
 }
