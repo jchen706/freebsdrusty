074358368103b542f7cdf20719f661966164913a
jun <junchen@151.com>
2020-04-12 09:52:44 -0400

    simple allocator

--- std/build.rs
+++ std/build.rs
@@ -54,59 +54,22 @@ fn main() {
     // to bindgen, and lets you build up options for
     // the resulting bindings.
     let bindings = bindgen::Builder::default()
+        .emit_builtins()
+         
+
         // The input header we would like to generate
         // bindings for.
         .header("wrapper.h")
         //clang args
-        .clang_arg("-O2")
-        .clang_arg("-pipe")
-        .clang_arg("-fno-strict-aliasing")
-        .clang_arg("-Werror")
-        .clang_arg("-D_KERNEL")
-        .clang_arg("-DKLD_MODULE")
-        .clang_arg("-nostdinc")
-        .clang_arg("-I.")
-        .clang_arg("-I/usr/src/sys")
-        .clang_arg("-fno-common")
-        .clang_arg("-fno-omit-frame-pointer")
-        .clang_arg("-mno-omit-leaf-frame-pointer")
-        .clang_arg("-MD")
-        .clang_arg("-mcmodel=kernel")
-        .clang_arg("-mno-red-zone")
-        .clang_arg("-mno-mmx")
-        .clang_arg("-mno-sse")
-        .clang_arg("-msoft-float")
-        .clang_arg("-fno-asynchronous-unwind-tables")
-        .clang_arg("-ffreestanding")
-        .clang_arg("-fwrapv")
-        .clang_arg("-fstack-protector")
-        .clang_arg("-Wall")
-        .clang_arg("-Wredundant-decls")
-        .clang_arg("-Wnested-externs")
-        .clang_arg("-Wstrict-prototypes")
-        .clang_arg("-Wmissing-prototypes")
-        .clang_arg("-Wpointer-arith")
-        .clang_arg("-Winline")
-        .clang_arg("-Wcast-qual")
-        .clang_arg("-Wundef")
-        .clang_arg("-Wno-pointer-sign")
-        .clang_arg("-D__printf__=__freebsd_kprintf__")
-        .clang_arg("-Wmissing-include-dirs")
-        .clang_arg("-fdiagnostics-show-option")
-        .clang_arg("-Wno-unknown-pragmas")
-        .clang_arg("-Wno-error-tautological-compare")
-        .clang_arg("-Wno-error-empty-body")
-        .clang_arg("-mno-aes")
-        .clang_arg("-mno-avx")
-        .clang_arg("-std=iso9899:1999")
         //need these because clang can't find some header files
         .clang_arg("sysroot=usr/include/")
         .clang_arg("-I/usr/include/")
-        .clang_arg("-target")
-        .clang_arg("aarch64-unknown-freebsd")
+   
+        .derive_debug(false)
         .opaque_type("timex")
         // Tell cargo to invalidate the built crate whenever any of the
         // included header files changed.
+        .rustfmt_bindings(true)
         .parse_callbacks(Box::new(bindgen::CargoCallbacks))
         // Finish the builder and generate the bindings.
         .generate()
